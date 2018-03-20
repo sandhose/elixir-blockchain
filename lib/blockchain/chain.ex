6f7d639a04daac4aa08a88c78f593e4451385074
Quentin Gliech <quentingliech@gmail.com>
2018-03-20 23:31:20 +0100

    Save chain blocks in ETS

--- lib/blockchain/chain.ex
+++ lib/blockchain/chain.ex
@@ -1,15 +1,34 @@
 defmodule Blockchain.Chain do
   alias Blockchain.Block
 
-  @type t :: [Blockchain.Block]
+  @table :blocks
 
-  def valid?(chain) do
-    Enum.reduce_while(chain, "", fn block, parent ->
-      if Block.valid?(block) && block.parent == parent do
-        {:cont, Block.hash(block)}
-      else
-        {:halt, nil}
-      end
-    end) != nil
+  def lookup(hash) do
+    case :ets.lookup(@table, hash) do
+      [{_hash, block}] -> block
+      _ -> nil
+    end
+  end
+
+  def insert(block) do
+    if Block.valid?(block) do
+      :ets.insert(@table, {Block.hash(block), block})
+      :ok
+    else
+      :error
+    end
+  end
+
+  def init do
+    :ets.new(@table, [:set, :protected, :named_table])
+  end
+
+  def valid?(block) do
+    cond do
+      block == nil -> false
+      not Block.valid?(block) -> false
+      block.parent == <<>> -> true
+      true -> valid?(lookup(block.parent))
+    end
   end
 end
