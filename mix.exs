3b1f06d2d12196a58747319a12039da1c4643699
Quentin Gliech <quentingliech@gmail.com>
2018-03-22 18:20:18 +0100

    Add ExCoveralls for test coverage

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Blockchain.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -23,7 +30,8 @@ defmodule Blockchain.MixProject do
   defp deps do
     [
       {:ed25519, "~> 1.2"},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.8", only: :test}
     ]
   end
 end
